96956fa93d71f15ff32a58866adf45c9dff8880a
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-11-06 12:25:19 +0600

    cors added for localhost data transfer

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("dotenv").config();
+var cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8snrbzq.mongodb.net/?retryWrites=true&w=majority`;
@@ -44,8 +46,13 @@ async function run() {
         .toArray();
       res.send(result);
     });
-    
-
+    // get single food details using id
+    app.get("/api/v1/foodDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allFoodsCollection.findOne(query);
+      res.send(result);
+    });
 
     // add food item api
     app.post("/api/v1/foodItems", async (req, res) => {
